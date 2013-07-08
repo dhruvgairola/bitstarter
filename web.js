e9786a0d8682eb0d8608742612391e23409cb6f9
dhruvgairola <dhruv.gairola.89@gmail.com>
2013-07-08 04:07:30 +0000

    Modified web.xml

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
+#!/usr/bin/env node
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+    response.send(data.toString('utf-8'));
+  });
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
